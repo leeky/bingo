8164875108efba4543b097ce7bbf368b2329f2b6
Robert Lee-Cann <leeky@leeky.org.uk>
2013-01-06 01:02:18 +0000

    Fix tests.

--- spec/controllers/fonts_controller_spec.rb
+++ spec/controllers/fonts_controller_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe FontsController do
   describe "#index" do
     it "renders shows a list of fonts" do
-      @font1 = FactoryGirl.create(:font, name: "Jenkins Bold")
-      @font2 = FactoryGirl.create(:font, name: "Blueberry Normal")
+      @font1 = FactoryGirl.create(:font, name: "Blueberry Normal")
+      @font2 = FactoryGirl.create(:font, name: "Jenkins Bold")
 
       get :index
       response.should render_template :index
--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "routing to the home page" do
   it "routes / to Homepage#index" do
-    get("/").should route_to("homepage#index")
+    get("/").should route_to("card_sets#index")
   end
 
   it "routes /fonts to Fonts#index" do
